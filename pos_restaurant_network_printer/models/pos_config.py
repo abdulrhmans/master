4705d9c42f439b95681696c25234f015a9125790
Jules <aimejules1@gmail.com>
2019-03-24 08:30:09 +0300

    Remove modules

--- pos_restaurant_network_printer/models/pos_config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-
-class PosConfig(models.Model):
-    _inherit = 'pos.config'
-
-    bill_print_mode = fields.Selection([('web', 'Web'), ('network', 'Network')], string='Bill print mode', default='web', required=True)
--- pos_restaurant_network_printer/models/restaurant_printer.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-
-class RestaurantPrinter(models.Model):
-    _inherit = 'restaurant.printer'
-
-    printer_port = fields.Integer(string="Port", required=True, default=9100)
